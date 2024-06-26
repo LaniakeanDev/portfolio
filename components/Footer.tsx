68619b84755f420d6eec8cfe4a00145aa3ae0281
LaniakeanDev <laniakean.dev@gmail.com>
2024-06-26 13:05:28 +0200

    first version, non responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,56 @@
 
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-pfBlue px-24 pt-12 pb-8 flex flex-col items-center">
+      <div className="flex justify-between w-full">
+        <div className="flex-1">
+          <p className="font-bold text-lg mb-4">Remy Zins</p>
+          <p className="max-w-64 text-sm">A FullStack developper crafting fully personalized web solutions with precision and passion</p>
+        </div>
+        <nav className="flex-1 grid place-items-center">
+          <ul className="gap-8 flex">
+            { NAV_LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={`footer-${link.key}`}
+                className="text-regular"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        <div className="flex-1 grid place-items-end">
+          <div>
+            <p className="w-full text-center mb-4 font-bold">Social</p>
+            <div className="flex gap-2">
+              <a href="https://www.linkedin.com/in/remyzins/" target="blank">
+                <Image
+                  src="/LinkedIn-bright.svg" 
+                  alt="LinkedIn"
+                  width={30}
+                  height={30}
+                />
+              </a>
+              <a href="https://github.com/LaniakeanDev" target="blank">
+                <Image
+                  src="/GitHub-bright.svg" 
+                  alt="GitHub"
+                  width={30}
+                  height={30}
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="text-sm">© Copyright 2024. Made by Remy Zins</p>
+    </footer>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,37 +4,39 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between flex-row py-5 px-14 items-center bg-pfBlue z-10 relative">
-      <Link href="/">
-        <div className="flex gap-4 items-center">
+    <header>
+      <nav className="flex justify-between flex-row py-5 px-14 items-center bg-pfBlue z-10 relative">
+        <Link href="/">
+          <div className="flex gap-4 items-center">
+          <Image 
+            src="/logo.png"
+            alt="logo"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+          <p>Remy Zins</p>
+          </div>
+        </Link>
+        <ul className="hidden h-full gap-12 sm:flex">
+          { NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
         <Image 
-          src="/logo.png"
-          alt="logo"
-          width={48}
-          height={48}
-          className="rounded-full"
+          src="burger.svg"
+          alt="burger menu icon"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer sm:hidden"
         />
-        <p>Remy Zins</p>
-        </div>
-      </Link>
-      <ul className="hidden h-full gap-12 sm:flex">
-        { NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
-      <Image 
-        src="burger.svg"
-        alt="burger menu icon"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer sm:hidden"
-      />
-    </nav>
+      </nav>
+    </header>
   )
 }
 
