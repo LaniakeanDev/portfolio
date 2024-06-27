19a14bee94b4eeb90f20659d61a350f36ef67916
LaniakeanDev <laniakean.dev@gmail.com>
2024-06-27 17:31:02 +0200

    responsive

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ import Button from "./Button"
 
 const About = () => {
   return (
-    <section className="full relative">
+    <section className="full relative pt-4 pb-16">
       <Image
         src="/about-bg.png" 
         alt="Fractal Background Image"
@@ -22,8 +22,8 @@ const About = () => {
           className="py-4"
         />
       </div>
-      <div className="flex px-10 gap-10 justify-around w-full mb-32">
-        <div className="max-w-[500px] flex flex-1 flex-col items-center">
+      <div className="flex flex-col items-center md:flex-row gap-10 justify-around w-full mb-32">
+        <div className="max-w-[500px] flex flex-1 flex-col items-center mx-[8vw] sm:mx-10">
           <h3 className="mb-8">Who am I?</h3>
           <p className="text-lg text-justify">
             I am a FullStack web developer, capable of building both the Front-End and the Back-End of your website or web application. You can check out some of my work in the Projects section.
@@ -33,7 +33,7 @@ const About = () => {
             I am open to Job opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience, don&#39;t hesitate to contact me.
           </p>
         </div>
-        <div className="max-w-[500px] flex flex-1 flex-col items-center">
+        <div className="max-w-[500px] flex flex-1 flex-col items-center mx-4">
           <h3 className="mb-8">My Stack</h3>
           <div className="max-w-[470px] flex justify-center gap-8 flex-wrap">
             <StackIcon id="Symfony" />
