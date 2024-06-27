8c23205177036eb25f0374c05ad3d84e88805a74
LaniakeanDev <laniakean.dev@gmail.com>
2024-06-27 18:16:25 +0200

    better responsive

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const Projects = () => {
   return (
-    <section className="full relative flex flex-col justify-center items-center p-10">
+    <section className="full relative flex flex-col justify-center items-center px-4 py-10 sm:p-10">
       <Image
         src="/projects-bg.png" 
         alt="Interface utilisateur pour La Tresse"
@@ -10,17 +10,17 @@ const Projects = () => {
         fill
       />
       <h2 className="mb-10">Projects</h2>
-      <p className="text-2xl text-center mb-10 lg:mb-40">An overview of a few projects I have worked on</p>
+      <p className="text-lg sm:text-2xl text-center mb-10 lg:mb-40">An overview of a few projects I have worked on</p>
       <div className="flex flex-col-reverse gap-10 lg:flex-row lg:gap-32 items-center">
-        <div className="h-80 w-[435px] relative">      
+        <div className="w-[300px] h-[220px] sm:h-80 sm:w-[435px] relative">      
           <Image
             src="/latresse.jpg" 
             alt="Fractal Background Image"
             fill
           />
         </div>
-        <div className="max-w-[458px]">
-          <h3>La Tresse</h3>
+        <div className="w-[90vw] max-w-[458px]">
+          <h3 className="mb-4">La Tresse</h3>
           <p>I designed and developed this tiny social network for members of a Third Place focusing on cooperation and social interactions.</p>
         </div>
       </div>
