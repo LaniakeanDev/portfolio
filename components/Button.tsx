f89d1980d35557fc2dffd4c0f46db7729debc922
LaniakeanDev <laniakean.dev@gmail.com>
2024-07-09 13:10:57 +0200

    fixed btn component

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,7 +11,7 @@ const Button = ({ type, title, /*icon,*/ variant }: ButtonProps) => {
     className={`flexCenter rounded-md ${variant} bg-pfYellow border-2 border-white text-pfDark px-16 py-2 cursor-pointer`}
       type={type}
     >
-      <label>{title}</label>      
+      {title}      
     </button>
   )
 }
