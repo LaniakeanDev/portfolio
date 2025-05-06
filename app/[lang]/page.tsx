9608a21c8b07ac7573eeddb256aaa47d4df868f0
LaniakeanDev <laniakean.dev@gmail.com>
2025-05-06 22:00:07 +0200

    fix without nested layout

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,22 +1,31 @@
+
+import { use } from 'react'
 import About from "@/components/About";
 import BackToTop from "@/components/BackToTop";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+type Params = Promise<{ lang: string }>
 
 
-
-
-export default async function Home({ params }: { params: { lang: string } }) {
-  const { lang } = await params;
+export default async function Home(props: {
+  params: Params
+}) {
+  const params = use(props.params)
+  const { lang } = params;
   const lg:supportedLanguages = lang === 'fr' ? 'fr' : 'en';
   return (
     <>
+      <Navbar lang={lg} />
       <Hero lang={lg} />
       <About lang={lg} />
       <Projects lang={lg} />
       <Contact lang={lg} />
       <BackToTop />
+      <Footer lang={lg} />
     </>
   );
 }
