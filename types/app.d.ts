51b0706fd60d24a1c2a813a3581096c44120018d
LaniakeanDev <laniakean.dev@gmail.com>
2025-10-16 08:16:52 +0200

    link component

--- types/app.d.ts
+++ types/app.d.ts
@@ -4,16 +4,35 @@ interface ILang {
   lang: supportedLanguages;
 }
 
+interface Img {
+  src: string;
+  alt: LocalizedText;
+  sizes?: string;
+  priority?: boolean;
+}
+
 type LocalizedText = { en: string; fr: string };
 
-interface IProject {
-  title: string;
-  description: {
-    en: string;
-    fr: string;
-  };
-  pictures: string[];
-  link?: string;
-  githubLink?: string;
-  year: string;
+// interface IProject {
+//   title: string;
+//   description: {
+//     en: string;
+//     fr: string;
+//   };
+//   pictures: string[];
+//   link?: string;
+//   githubLink?: string;
+//   year: string;
+// }
+
+interface ILink {
+  href: string;
+  label?: string;
+  icon?: SVGComponent;
+  iconClassName?: string;
+  alt?: string;
+  children?: React.ReactElement;
+  className?: string;
+  opensInNewTab?: boolean;
+  inline?: boolean;
 }
