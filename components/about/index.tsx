a69b81041dfbc214006fa30da068b4bdd3ef8122
LaniakeanDev <laniakean.dev@gmail.com>
2025-10-21 16:21:26 +0200

    about update

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -1,28 +1,26 @@
 import PageSection from '../section';
-import { content, whyList } from './content';
+import { content /*whyList */ } from './content';
 import Title from '../title';
 
 export default function About({ lang }: ILang) {
   return (
     <PageSection title={content.title[lang]}>
       <div className="max-w-4xl">
-        <Title level="h3" titleClassName="text-3xl my-4">
+        {/* <Title level="h3" titleClassName="text-3xl my-4">
           {content.who[lang]}
+        </Title> */}
+
+        {/* <div className="bg-white/10 rounded-lg mb-16 p-12"> */}
+        <p className="mb-4 xl:text-xl">{content.iam[lang]}</p>
+        <p className="mb-16 xl:text-xl">{content.iam2[lang]}</p>
+        <Title level="h4" titleClassName="text-2xl xl:text-3xl my-4">
+          {content.journeyH4[lang]}
         </Title>
+        <p className="mb-4 xl:text-xl">{content.journeyContent.p1[lang]}</p>
+        <p className="xl:text-xl">{content.journeyContent.p2[lang]}</p>
+        {/* </div> */}
 
-        <div className="bg-white/10 rounded-lg mb-16 p-12">
-          <p className="mb-4">{content.iam[lang]}</p>
-          <p className="mb-8">{content.iam2[lang]}</p>
-          {/* <p className='mb-4'>{content.iam2[lang]}</p>
-          <p className='mb-8'>{content.iam3[lang]}</p> */}
-          <Title level="h4" titleClassName="text-2xl my-4">
-            {content.journeyH4[lang]}
-          </Title>
-          <p className="mb-4">{content.journeyContent.p1[lang]}</p>
-          <p>{content.journeyContent.p2[lang]}</p>
-        </div>
-
-        <Title level="h3" titleClassName="text-3xl my-4">
+        {/* <Title level="h3" titleClassName="text-3xl my-4">
           {content.whyH3[lang]}
         </Title>
 
@@ -40,7 +38,7 @@ export default function About({ lang }: ILang) {
               </li>
             ))}
           </ul>
-        </div>
+        </div> */}
       </div>
     </PageSection>
   );
