81d3309d3a2e1a12dbefaa42233ba886dd457189
LaniakeanDev <laniakean.dev@gmail.com>
2025-10-23 17:11:25 +0200

    services

--- components/services/service-card/index.tsx
+++ components/services/service-card/index.tsx
@@ -11,13 +11,13 @@ interface ServiceCardProps {
 
 export default function ServiceCard({ icon, title, description, list /* lang */ }: ServiceCardProps) {
   return (
-    <li className="rounded-2xl py-8 px-12 w-[95vw] max-w-[400px] h-[450px] select-none hover-scale card">
+    <li className="rounded-2xl p-8 w-[95vw] max-w-md min-h-[500px] select-none hover-scale card">
       <IconComponent name={icon} className="w-12 h-12 mb-4" />
       <Title level="h3" titleClassName="text-2xl mt-4 mb-6 font-semibold text-(--gold)">
         {title}
       </Title>
-      <p className="mb-4">{description}</p>
-      <ul className="list-disc mx-4">
+      <p className="mb-8 mx-4">{description}</p>
+      <ul className="list-disc mx-8">
         {list.map((item, idx) => (
           <li key={`${title}-list-${String(idx)}`}>
             <p>{item}</p>
