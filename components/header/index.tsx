77c7e250452d488aa79b2641309b93929eedc0c5
LaniakeanDev <laniakean.dev@gmail.com>
2025-10-23 17:26:24 +0200

    theme toggle

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,6 +3,7 @@ import { InternalLink } from '../link';
 import Navbar from './navbar';
 import { navData } from './content';
 import BurgerMenu from './burger';
+import ThemeToggle from '../ThemeToggle';
 
 export default function Header({ lang }: ILang) {
   return (
@@ -19,8 +20,9 @@ export default function Header({ lang }: ILang) {
             </p>
           </div>
         </InternalLink>
-        <Navbar menuItems={navData[lang]} className="!hidden lg:!flex" />
-        <BurgerMenu menuItems={navData[lang]} className="z-20 lg:!hidden" />
+        <Navbar menuItems={navData[lang]} className="hidden! lg:flex!" />
+        <BurgerMenu menuItems={navData[lang]} className="z-20 lg:hidden!" />
+        <ThemeToggle className="mr-8 sm:mr-0 sm:ml-8 md:ml-16" />
       </div>
     </header>
   );
